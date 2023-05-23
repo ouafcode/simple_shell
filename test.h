6b3e06605b733912eaf8181d50fd3a4fddc17b2a
aminem27 <amine_abbes@hotmail.com>
2023-05-23 14:53:01 -0700

    test files removed

--- test.h
+++
@@ -1,8 +0,0 @@
-#ifndef __TEST_H__
-#define __TEST_H__
-#include <stdio.h>
-#include <stdlib.h>
-char **_strtok(char *str, char *d);
-char *get_cmd(char *str);
-int delim_check(char c, char *d);
-#endif
